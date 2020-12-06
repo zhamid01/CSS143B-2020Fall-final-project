55a0e38d0af13b9a17bb330003361be6b0ad3e40
zhamid01 <zhamid01@uw.edu>
2020-12-05 23:48:08 -0800

    Working on IndexerImpl

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -11,7 +11,25 @@ import java.util.Map;
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
-        // add your code
+        if (docs != null) {
+            List<List<Integer>> list1 = new ArrayList<>();
+            for (int i = 0; i < docs.size(); i++) {
+                String n1 = docs.get(i);
+                List<Integer> list = new ArrayList<Integer>();
+                n1.split(" ");
+                for (int k = 0; k < n1.length(); k++) {
+                    String m1 = n1;
+                    int count = 0;
+                    for (int j = 0; j < n1.length(); j++) {
+                        if (m1 == n1) {
+                            list.add(i);
+                        }
+                    }
+                    list1.add(list);
+                }
+                indexes.put(n1, list1);
+            }
+        }
         return indexes;
     }
 }
