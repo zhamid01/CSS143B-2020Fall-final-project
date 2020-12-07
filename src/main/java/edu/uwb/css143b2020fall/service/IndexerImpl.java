92ea0e65b4fe49b183b7e9f5a7d8b6c8265c156d
zhamid01 <zhamid01@uw.edu>
2020-12-06 17:24:05 -0800

    Fixed ArrayList placement

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -15,19 +15,17 @@ public class IndexerImpl implements Indexer {
             List<List<Integer>> list1 = new ArrayList<>();
             for (int i = 0; i < docs.size(); i++) {
                 String n1 = docs.get(i);
-                List<Integer> list = new ArrayList<Integer>();
                 String[] n2 = n1.split(" ");
-                for (String s : n2) {
                     for (int j = 0; j < n2.length; j++) {
+                        List<Integer> list = new ArrayList<Integer>();
                         for (int k = 0; k < n2.length; k++) {
                             if (n2[j] == n2[k]) {
                                 list.add(j);
                             }
                         }
                         list1.add(list);
+                        indexes.put(n2[j], list1);
                     }
-                    indexes.put(s, list1);
-                }
             }
         }
         return indexes;
