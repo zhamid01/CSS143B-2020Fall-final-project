f4fd6d57e2a07f78d5a64ce30b084f4af8b7b8fc
zhamid01 <zhamid01@uw.edu>
2020-12-06 17:16:51 -0800

    Working on IndexerImpl

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -17,15 +17,17 @@ public class IndexerImpl implements Indexer {
                 String n1 = docs.get(i);
                 List<Integer> list = new ArrayList<Integer>();
                 String[] n2 = n1.split(" ");
+                for (String s : n2) {
                     for (int j = 0; j < n2.length; j++) {
                         for (int k = 0; k < n2.length; k++) {
                             if (n2[j] == n2[k]) {
                                 list.add(j);
                             }
-                            list1.add(list);
                         }
-                        indexes.put(n2[j], list1);
+                        list1.add(list);
                     }
+                    indexes.put(s, list1);
+                }
             }
         }
         return indexes;
