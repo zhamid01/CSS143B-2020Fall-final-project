8bf7dd120d5a573fc56f1d44ad8c40c4b102d727
zhamid01 <zhamid01@uw.edu>
2020-12-06 19:26:55 -0800

    Changing while to if

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -12,20 +12,20 @@ public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
         if (docs != null) {
-            List<List<Integer>> list1 = new ArrayList<>();
             for (int i = 0; i < docs.size(); i++) {
                 String n1 = docs.get(i);
                 String[] n2 = n1.split(" ");
-                    for (int j = 0; j < n2.length; j++) {
-                        List<Integer> list = new ArrayList<Integer>();
-                        for (int k = 0; k < n2.length; k++) {
-                            if (n2[j] == n2[k]) {
-                                list.add(j);
-                            }
+                List<List<Integer>> list1 = new ArrayList<>();
+                for (int j = 0; j < n2.length; j++) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    for (int k = 0; k < n2.length; k++) {
+                        if (n2[j] == n2[k]) {
+                            list.add(j);
                         }
-                        list1.add(list);
-                        indexes.put(n2[j], list1);
                     }
+                    list1.add(list);
+                    indexes.put(n2[j], list1);
+                }
             }
         }
         return indexes;
