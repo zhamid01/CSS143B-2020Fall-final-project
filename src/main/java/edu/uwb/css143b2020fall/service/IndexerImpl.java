470d8a775147b30f0e408ba14cd6d68542d8fc42
zhamid01 <zhamid01@uw.edu>
2020-12-06 23:22:18 -0800

    Commit 12/6

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,10 +2,7 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class IndexerImpl implements Indexer {
@@ -16,18 +13,48 @@ public class IndexerImpl implements Indexer {
                 String n1 = docs.get(i);
                 String[] n2 = n1.split(" ");
                 List<List<Integer>> list1 = new ArrayList<>();
-                for (int j = 0; j < n2.length; j++) {
+                HashMap<String, String> s1 = new HashMap<>();
+                for (int m = 0; m < n2.length; m++) {
+                    s1.put(n2[m], n2[m]);
+                }
+                Collection<String> l = s1.values();
+                String[] s2 = l.toArray(new String[0]);
+                for (int j = 0; j < s2.length; j++) {
                     List<Integer> list = new ArrayList<Integer>();
                     for (int k = 0; k < n2.length; k++) {
-                        if (n2[j] == n2[k]) {
-                            list.add(j);
+                        if (n2[j].equals(" ")) {
+                            continue;
+                        }
+                        if (s2[j] == n2[k]) {
+                            list.add(k);
                         }
                     }
                     list1.add(list);
-                    indexes.put(n2[j], list1);
+                    indexes.put(s2[j], list1);
                 }
             }
         }
         return indexes;
     }
 }
+
+
+
+
+                    /*
+                    if (s1.put(n2[j], 1) == null) {
+                        continue;
+                    }
+                    if (n2[j].equals("")) {
+                        continue;
+                    }
+                    List<Integer> list = new ArrayList<Integer>();
+                    for (int k = 0; k < n2.length; k++) {
+                        if (n2[j] == n2[k]) {
+                            list.add(k);
+                        }
+                    }
+                    list1.add(list);
+                    indexes.put(n2[j], list1);
+                }
+                     */
